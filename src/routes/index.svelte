9fc461575b450ed9047f8d591cb7d9c38f05bd43
alsino <iamahorse@gmail.com>
2021-06-07 11:45:17 +0200

    video added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,13 +30,22 @@
     <source src="output.mp4" type="video/mp4">
   </video> -->
 
-  <iframe 
+  <!-- <iframe 
     src="https://player.vimeo.com/video/554946488" 
     width="640" 
     height="360" 
     frameborder="0" 
     allow="autoplay; fullscreen; picture-in-picture" 
     allowfullscreen>
+</iframe> -->
+
+<iframe 
+src="https://player.vimeo.com/video/559875618?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+frameborder="0" 
+allow="autoplay; fullscreen; picture-in-picture" 
+allowfullscreen 
+style="position:absolute;top:0;left:0;width:100%;height:100%;" 
+title="TPHC-docu - HD 720p.mov">
 </iframe>
 
 </div>
