72f29fddc8f5bf09a0f310e37fbb748359c69023
alsino <iamahorse@gmail.com>
2022-03-11 12:49:13 +0100

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,7 @@ import adapter from '@sveltejs/adapter-netlify';
 
 const config = {
 	kit: {
-		adapter: adapter(), // currently the adapter does not take any options
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
